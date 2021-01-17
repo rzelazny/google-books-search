a3f26e33fedb6d2306ee25f45b04514b2bf3b029
Ryan Zelazny <RyanZelazny@gmail.com>
2021-01-16 20:37:20 -0500

    home page css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
